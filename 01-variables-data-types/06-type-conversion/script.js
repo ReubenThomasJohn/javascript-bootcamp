dec16eb7626d3aeb19e8173861f55c213f25ce7a
reuben <reubenthomas.john2017@gmail.com>
2023-03-23 15:16:03 +0530

    adding more lessons

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,24 @@
+let amount = 'hello';
+
+// change string to number
+
+amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+
+// change number to string
+// amount = amount.toString();
+// amount = String(amount);
+
+// change string to decimal
+// amount = parseFloat(amount);
+
+// convert number to boolean
+// amount = Boolean(amount);
+
+console.log(Math.sqrt(-1));
+console.log(1+NaN);
+console.log(undefined+undefined);
+console.log('foo' / 3);
+
+console.log(amount, typeof(amount));
---
+++ 01-variables-data-types/07-operators/script.js
@@ -0,0 +1,47 @@
+// 1. Arithmetic Operators
+
+let x;
+
+x = 5+5;
+x = 5-5;
+x = 5/5;
+
+// Con
+x = 'Hello' + ' ' + 'World';
+
+// Exponent
+x = 2 ** 3
+
+// Increment
+x = 1;
+x++;
+
+// Decrement
+x = 4;
+x--;
+
+// Assignment
+
+x = 10;
+
+x += 5;
+x *= 20;
+x /= 30;
+x %= 5;
+x **= 5;
+
+// 3. Comparison Operators
+
+x = 2 == 2; // checks only type inside, and not the type
+x = 2 === '2'; // checks the type and value
+
+x = 2 != '2';
+x = 2 !== '2';
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 10;
+
+
+console.log(x);
