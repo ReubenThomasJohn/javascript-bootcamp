a59773d1b4c53f838543dd9759f8ad57c7e8993f
reuben <reubenthomas.john2017@gmail.com>
2023-03-27 13:42:12 +0530

    adding lessons

---
+++ 01-variables-data-types/12-math-objects/script.js
@@ -0,0 +1,23 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.2);
+
+x = Math.ceil(4.2);
+
+x = Math.floor(4.9);
+
+x = Math.pow(2, 3);
+
+x = Math.min(4, 5);
+
+x = Math.max(4, 5, 6);
+
+x = Math.random();
+
+let y = Math.floor(Math.random() * 100 + 1);
+
+console.log(y);
